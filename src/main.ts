bbbaf97ab4ce971ac18df23c5b711ad9c9420bf6
Peter Curth <peter@curth.de>
2020-05-11 20:17:15 +0200

    Small adjustments

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,7 @@ declare global {
 	namespace ioBroker {
 		interface AdapterConfig {
 			// Define the shape of your options here (recommended)
-			option1: boolean;
-			option2: string;
+			senseboxid: string;
 			// Or use a catch-all approach
 			[key: string]: any;
 		}
@@ -46,8 +45,7 @@ class Template extends utils.Adapter {
 
 		// The adapters config (in the instance object everything under the attribute "native") is accessible via
 		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
+		this.log.info("config option1: " + this.config.senseboxid);
 
 		/*
 		For every state in the system there has to be also an object of type state
